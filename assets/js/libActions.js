20c49a9e1effded3c0ff9bca650f6ba9f6c01bf7
Dmitry <sidelevdima@yandex.ua>
2019-10-18 10:09:11 +0300

    add each for checkboxes

--- assets/js/libActions.js
+++ assets/js/libActions.js
@@ -32,10 +32,31 @@ $(function () {
     });
     // add new books
     $(document).on('click', '#addBooks', function () {
+        // get books modal
         $.get('/library/book_list_to_add', { id: libId })
             .done(function (data) {
                 modalPlaceholder.html(data);
                 modalPlaceholder.find('.modal').modal('show');
             });
+        // disable or enable add button
+        modalPlaceholder.on('change', '#addCheckbox', function () {
+            let saveBtn = modalPlaceholder.find('[data-save="modal"]');
+            if ($(this).is(':checked')) {
+                saveBtn.removeAttr('disabled');
+            } else {
+                saveBtn.attr('disabled', true);
+            }
+        });
+        modalPlaceholder.on('click', '[data-save="modal"]', function () {
+            let table = modalPlaceholder.find('#booksToAdd');
+            let bookIdsToAdd;
+            table.find('.form-check-input').each(function (i, el) {
+               if (el.checked)
+               {
+                   bookIdsToAdd = el.dataset['bookId'];
+               }
+            });
+            console.log(bookIdsToAdd);
+        });
     });
 });
